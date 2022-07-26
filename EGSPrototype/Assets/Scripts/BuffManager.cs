d3d9071669d5beea75f387016455f4500cb3158e
Quan-Ngo <71655929+Quan-Ngo@users.noreply.github.com>
2022-07-25 23:32:57 -0400

    Added buff buttons and implemented win menu displaying random buttons

---
+++ EGSPrototype/Assets/Scripts/BuffManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuffManager : MonoBehaviour
+{    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    
+	
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void giveDamageBuff(){
+	//give player dmgbuff
+    }
+	
+	public void giveParryBuff(){}
+	
+	public void giveHealBuff(){}
+	
+	public void giveSpeedBuff(){}
+	
+	public void giveBackstabBuff(){}
+	
+	public void giveDodgeBuff(){}
+	
+	public void giveMeterBuff(){}
+	
+	public void giveChargeAttackBuff(){}
+	
+	public void giveRangeBuff(){}
+}
---
+++ EGSPrototype/Assets/Scripts/WinMenu.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinMenu : MonoBehaviour
+{
+	public Transform buttonLocationL;
+	public Transform buttonLocationM;
+	public Transform buttonLocationR;
+	public GameObject[] buffButtons;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+	
+	void Awake(){
+		GameObject buffButton1;
+		GameObject buffButton2;
+		GameObject buffButton3;
+		int buttonSelection;
+		ArrayList selectedButtons = new ArrayList();
+
+		while(selectedButtons.Count < 3){
+			buttonSelection = Random.Range(0, buffButtons.Length);
+			if (!selectedButtons.Contains(buttonSelection)){
+				selectedButtons.Add(buttonSelection);
+			}
+		}	
+
+		buffButton1 = Instantiate<GameObject>(buffButtons[(int) selectedButtons[0]].gameObject, buttonLocationL);
+		buffButton1.transform.position = buttonLocationL.position;
+		
+		buffButton2 = Instantiate<GameObject>(buffButtons[(int) selectedButtons[1]].gameObject, buttonLocationM);
+		buffButton2.transform.position = buttonLocationM.position;
+		
+		buffButton3 = Instantiate<GameObject>(buffButtons[(int) selectedButtons[2]].gameObject, buttonLocationR);
+		buffButton3.transform.position = buttonLocationR.position;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
